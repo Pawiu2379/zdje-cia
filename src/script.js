7c01ea3a9e89b54cd5f01d249019f151bc493b0f
Pawiu__ <pawelkoc681@gmail.com>
2022-10-18 09:41:54 +0200

    prawie działa

--- src/script.js
+++ src/script.js
@@ -1,24 +1,31 @@
+const zdjecia =["src/imgs/image1.png","src/imgs/image2.png","src/imgs/image3.png","src/imgs/image4.png","src/imgs/image5.png"]
+
+let index = 0
+let adsd = setInterval(defult,5000)
 
 function defult(){
-
-    var img = 1
-    
-
-
-
-    function changeimg(){
-        img++
-        console.log(img)
-        document.getElementById('pokemon').src=`./src/imgs/image${img}.png`
-
-        if(file = 5){
-            file = 0
-        }
+    document.getElementById('pokemon').src=zdjecia[index]
+    index++
+    if(index>4){
+        index=0
     }
-
-    setTimeout(changeimg(), 500, false)
-
 }
 
 
-window.onload= defult()
+document.getElementById('next').addEventListener('click',()=>{
+    index+1
+    document.getElementById('pokemon').src=zdjecia[index]
+    
+})
+document.getElementById('previous').addEventListener('click',()=>{
+    index-1
+    document.getElementById('pokemon').src=zdjecia[index]
+})
+
+document.getElementById('stop').addEventListener('click',()=>{
+    clearInterval(adsd)
+})
+
+document.getElementById('start').addEventListener('click',()=>{
+    let adsd = setInterval(defult,500)
+})
