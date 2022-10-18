8f9c0fa402057b674b5eb0647d2b2fe1e88bba1a
Pawiu__ <pawelkoc681@gmail.com>
2022-10-18 19:34:55 +0200

    dasdsa

--- src/script.js
+++ src/script.js
@@ -1,31 +1,36 @@
-const zdjecia =["src/imgs/image1.png","src/imgs/image2.png","src/imgs/image3.png","src/imgs/image4.png","src/imgs/image5.png"]
+const zdjecia = ["src/imgs/image1.png", "src/imgs/image2.png", "src/imgs/image3.png", "src/imgs/image4.png", "src/imgs/image5.png"]
 
 let index = 0
-let adsd = setInterval(defult,5000)
+let adsd = setInterval(defult, 500)
 
-function defult(){
-    document.getElementById('pokemon').src=zdjecia[index]
+function defult() {
+    document.getElementById('pokemon').src = zdjecia[index]
     index++
-    if(index>4){
-        index=0
+    if (index > 4) {
+        index = 0
     }
 }
 
+document.getElementById('next').addEventListener('click', () => {
+    index + 1
+    document.getElementById('pokemon').src = zdjecia[index]
 
-document.getElementById('next').addEventListener('click',()=>{
-    index+1
-    document.getElementById('pokemon').src=zdjecia[index]
-    
 })
-document.getElementById('previous').addEventListener('click',()=>{
-    index-1
-    document.getElementById('pokemon').src=zdjecia[index]
-})
-
-document.getElementById('stop').addEventListener('click',()=>{
-    clearInterval(adsd)
+document.getElementById('previous').addEventListener('click', () => {
+    index - 1
+    document.getElementById('pokemon').src = zdjecia[index]
 })
 
-document.getElementById('start').addEventListener('click',()=>{
-    let adsd = setInterval(defult,500)
+
+
+
+document.getElementById('pokemon').addEventListener('click', () => {
+    var check = document.getElementById('picture');
+    if (check.checked) {
+        let asds = setInterval(defult, 500)
+    } else {
+        clearInterval(adsd)
+    }
+
+
 })
